c10535bacf71578a32d817fc184c1e4588d65c27
Frankosviz <geom.francescopaolo@gmail.com>
2024-05-02 23:27:28 +0200

    php - hotel : Coding

--- Views/main.php
+++ Views/main.php
@@ -1,18 +1,25 @@
 <?php
+$template = "";
+foreach ($hotels as $hotel) {
+    $template .= "<tr> <td>{$hotel['name']}</td> <td>{$hotel['description']}</td> <td>{$hotel['parking']}</td> <td>{$hotel['vote']}</td> <td>{$hotel['distance_to_center']}</td> </tr>";
 
-for ($i = 0; $i < count($hotels); $i++) {
-    $templateHotel = $hotels[$i];
-    
-    echo "<div class='container'>"; 
-    echo $templateHotel; 
-    echo "</div>";
 }
+
 
 ?>
 
-<body>
-    
-    <div class="container">
-        <?php echo $templateHotel; ?>
-    </div>
-</body>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">name</th>
+            <th scope="col">description</th>
+            <th scope="col">parking</th>
+            <th scope="col">vote</th>
+            <th scope="col">distance to center</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?= $template ?>
+
+    </tbody>
+</table>
